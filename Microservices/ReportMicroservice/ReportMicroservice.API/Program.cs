587308ffcc44fe02d50e6cbc71e35265f7c1f8bf
şükrü hasta <sukruhasta95@gmail.com>
2024-06-19 20:33:29 +0300

    microservices-layers-setup migrations

--- Microservices/ReportMicroservice/ReportMicroservice.API/Program.cs
+++ Microservices/ReportMicroservice/ReportMicroservice.API/Program.cs
@@ -1,12 +1,15 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using ReportMicroservice.Infrastructure.Concrete.EntityFramework.Context;
 
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
